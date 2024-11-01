f4e8ca10f00389c070fb0a2ebe5e6bc3600d75f1
aycode7 <159772484+aycode7@users.noreply.github.com>
2024-11-01 01:16:37 +0100

    excluding preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,12 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Homepage from '../components/Homepage'
-import Loader from '@/components/Loader';
 
 const Page = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(()=>{
-      setLoading(false);
-    }, 2000)
-  }, []);
 
   return (
     <div>
-      {loading ? <Loader />:<Homepage />}
+      <Homepage />
       
     </div>
   )
